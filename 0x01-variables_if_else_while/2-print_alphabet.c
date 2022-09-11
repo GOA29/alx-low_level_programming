0164760b7e31e21a63b3972a33e502b8159a1775
GOA29 <goldenbabe2905@gamil.com>
2022-09-11 14:32:10 -0700

    My Variables Commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
